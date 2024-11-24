5e40c12d4c5631e4c23fdba1c892dd60d54980d2
SomratIIT <101197949+SomratIIT@users.noreply.github.com>
2024-11-24 12:27:07 +0600

    Working On transaction Part

---
+++ js/featues.js
@@ -0,0 +1,6 @@
+document.getElementById('donation-button').addEventListener('click',function(){
+    showSectionById('cash-out-form');
+});
+document.getElementById('show-transactions-form').addEventListener('click',function(){
+    showSectionById('transaction-form');
+});
--- js/utilities.js
+++ js/utilities.js
@@ -15,3 +15,12 @@ function getInputFieldValueById(id){
     const textNumber = parseFloat(textValue);
     return textNumber;
   }
+
+
+  function showSectionById(id){
+
+    document.getElementById('add-money-form').classList.add('hidden');
+    document.getElementById('transaction-form').classList.add('hidden');
+  
+    document.getElementById(id).classList.remove('hidden');
+  }
